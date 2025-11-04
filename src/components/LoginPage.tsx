2919cb56ad057136879a93feccf1f4793fbc16ce
mateoreyes10 <150271611+mateoreyes10@users.noreply.github.com>
2025-11-04 09:03:40 -0500

    Correccion

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -17,15 +17,15 @@ const LoginPage = ({ onLogin }: LoginPageProps) => {
 
     try {
       // 1. Fetch al backend
-      const response = await fetch("http://localhost:5000/api/auth/google", {
+      /*const response = await fetch("http://localhost:5000/api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ token: idToken }),
-      });
+      });*/
 
-      /* const response = await fetch(
+      const response = await fetch(
         "https://lang-mate-match.onrender.com/api/auth/google",
         {
           method: "POST",
@@ -34,7 +34,7 @@ const LoginPage = ({ onLogin }: LoginPageProps) => {
           },
           body: JSON.stringify({ token: idToken }),
         }
-      );aa*/
+      );
 
       if (!response.ok) {
         throw new Error("Fallo la verificación del token en el backend.");

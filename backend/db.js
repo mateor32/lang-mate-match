9714532a8c2bf83130b66546420cb0ce60eeb7df
mateoreyes10 <150271611+mateoreyes10@users.noreply.github.com>
2025-11-04 12:31:24 -0500

    Base de datos 2

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 // backend/db.js
-import pkg from "pg";
+/*import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
@@ -8,4 +8,20 @@ export const pool = new Pool({
   database: "linguamatch",
   password: "password",
   port: 5432,
+});*/
+
+// backend/db.js
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://linguamatch_db_user:VtnoTmkIQd4yKOGd9R1i3LeLqCGO7ugz@dpg-d41rqj6uk2gs738p06h0-a.oregon-postgres.render.com/linguamatch_db",
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false } // Render requiere SSL
+    : false, // Local no usa SSL
 });
